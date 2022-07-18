a19326e1c0d0ae72a781ce6a80acd1bb7dee8c2f
Fuad Крым <fuadkrym@gmail.com>
2022-07-18 14:32:20 +0300

    feat: add data

---
+++ src/data/data.ts
@@ -0,0 +1,146 @@
+export interface IData {
+	id: number;
+	amount: number;
+	name: string;
+	date:string;
+	color: string;
+	popular:boolean;
+	manufacturer:string;
+	price:string;
+	url:string
+  }
+export let data:IData []=[
+	{
+		id:1,
+		amount:2,
+		name:'Xiaomi 12 pro',
+		date:'2000',
+		color:'black',
+		popular:true,
+		manufacturer:'Xiaomi',
+		price:'20 670',
+		url:'assets/Xiaomi12pro.png'
+	},
+	{
+		id:2,
+		amount:6,
+		name:'Xiaomi POCO F1',
+		date:'2000',
+		color:'black',
+		popular:false,
+		manufacturer:'Xiaomi',
+		price:'20 670',
+		url:'assets/XiaomiPocophoneF1.jpg'
+	},
+	{
+		id:3,
+		amount:10,
+		name:'Xiaomi 12X',
+		date:'2000',
+		color:'black',
+		popular:true,
+		manufacturer:'Xiaomi',
+		price:'20 670',
+		url:'assets/Xiaomi12X.webp'
+	},
+	{
+		id:4,
+		amount:2,
+		name:'Xiaomi 11T ',
+		date:'2000',
+		color:'black',
+		popular:false,
+		manufacturer:'Xiaomi',
+		price:'20 670',
+		url:'assets/Xiaomi11T.webp'
+	},
+	{
+		id:5,
+		amount:12,
+		name:'Samsung Galaxy A02',
+		date:'2000',
+		color:'red',
+		popular:false,
+		manufacturer:'Samsung',
+		price:'20 670',
+		url:'assets/SamsungGalaxyA02.jpg'
+	},
+	{
+		id:6,
+		amount:1,
+		name:'Samsung Galaxy S22',
+		date:'2000',
+		color:'black',
+		popular:false,
+		manufacturer:'Samsung',
+		price:'20 670',
+		url:'assets/SamsungGalaxyS22.jpg'
+	},
+	{
+		id:7,
+		amount:4,
+		name:'Samsung Galaxy S10',
+		date:'2000',
+		color:'red',
+		popular:false,
+		manufacturer:'Samsung',
+		price:'20 670',
+		url:'assets/SamsungGalaxyS10e.webp'
+	},
+	{
+		id:8,
+		amount:9,
+		name:'Samsung Galaxy A12',
+		date:'2000',
+		color:'blue',
+		popular:true,
+		manufacturer:'Samsung',
+		price:'20 670',
+		url:'assets/SamsungGalaxyA12.jpg'
+	},
+	{
+		id:9,
+		amount:10,
+		name:'IPhone 7 Plus',
+		date:'2000',
+		color:'red',
+		popular:false,
+		manufacturer:'iPhone',
+		price:'20 670',
+		url:'assets/IPhone7Plus.jpg'
+	},
+	{
+		id:10,
+		amount:2,
+		name:'IPhone 12',
+		date:'2000',
+		color:'blue',
+		popular:false,
+		manufacturer:'iPhone',
+		price:'20 670',
+		url:'assets/IPhone12.webp'
+	},
+	{
+		id:11,
+		amount:12,
+		name:'IPhone 13',
+		date:'2000',
+		color:'blue',
+		popular:true,
+		manufacturer:'iPhone',
+		price:'20 670',
+		url:'assets/IPhone13.png'
+	},
+	{
+		id:12,
+		amount:23,
+		name:'IPhone Xr',
+		date:'2000',
+		color:'red',
+		popular:true,
+		manufacturer:'iPhone',
+		price:'20 670',
+		url:'assets/IPhoneXr.webp'
+	},
+]
+
